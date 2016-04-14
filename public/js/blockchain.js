19961778c8c02f54bce3ecd195f6b01d1a414dd6
Rouan van der Ende <rouan@8bo.org>
2016-04-14 14:49:53 +0200

    unconfirmed tx

--- public/js/blockchain.js
+++ public/js/blockchain.js
@@ -9,6 +9,7 @@ var blockchainListen = function(walletaddress, callback) {
 		websocket.onopen = function(e) { 
 			var message = {}
 			
+			//subscribe to own account client side
 			message.op = "addr_sub"
 			message.addr = walletaddress
 			
@@ -18,10 +19,16 @@ var blockchainListen = function(walletaddress, callback) {
 		};
 
 		websocket.onclose = function(e)   { 
+			//console.log("connection closed")
 			connect();
 		};
 
-		websocket.onmessage = function(message) { callback(message) };
+		websocket.onmessage = function(message) { 
+			var parsed = JSON.parse(message.data);
+			//console.log(parsed);
+			callback(parsed)
+		};
+
 		websocket.onerror = function(err)   { console.log(err) };
 	}
 
